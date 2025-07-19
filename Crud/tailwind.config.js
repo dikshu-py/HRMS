e07ab38280d81122eacf9cb09d4db33c70108351
Dikshant <dikshantparasar@gmail.com>
2025-07-19 08:14:23 +0530

    tailwind Configs

---
+++ Crud/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // adjust to your project
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4D007D", // custom name and value
+      },
+    },
+  },
+  plugins: [],
+};
