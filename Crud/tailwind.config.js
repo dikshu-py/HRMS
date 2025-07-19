e43ac71b8e6b8798adad0e427cb73b24e896cae4
Dikshant <dikshantparasar@gmail.com>
2025-07-19 23:11:15 +0530

    All Files to Server

--- Crud/tailwind.config.js
+++ Crud/tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"], // adjust to your project
   theme: {
     extend: {
+      animation: {
+        fade: 'fadeOut 5s ease-in-out',
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeOut: {
+          '0%': { backgroundColor: theme('colors.red.300') },
+          '100%': { backgroundColor: theme('colors.transparent') },
+        },
+      }),
       colors: {
         primary: "#4D007D", // custom name and value
       },
