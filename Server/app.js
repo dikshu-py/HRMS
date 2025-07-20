2f5308b05010ca58192ad616b4be021d2daf13bc
DIKSHANT पाराशर <60055818+dikshu-py@users.noreply.github.com>
2025-07-20 17:32:07 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -43,6 +43,11 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // Connect to MongoDB
