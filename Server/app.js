e1d0a2acc8fefbe8aee40c860372a93002fb634f
Dikshant <dikshantparasar@gmail.com>
2025-07-20 19:34:37 +0530

    app error

--- Server/app.js
+++ Server/app.js
@@ -39,7 +39,7 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-};
+}));
 
 // ✅ USE THIS AS GLOBAL MIDDLEWARE
 app.use(cors(corsOptions));
