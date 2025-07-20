290b6c3a0e01804aafcf28c425bd9cdfcc2bd371
DIKSHANT पाराशर <60055818+dikshu-py@users.noreply.github.com>
2025-07-20 19:32:33 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 const allowedOrigins = [
   'http://localhost:5173',
   'https://hrms-portal-aa2ldavyp-dikshu-pys-projects.vercel.app',
-  'https://hrms-portal-fawn.vercel.app/login'
+  'https://hrms-portal-fawn.vercel.app'
 ];
 
 app.use(cors({
