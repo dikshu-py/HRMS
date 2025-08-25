e8e61c945e27cc90e88f9fde4c245661690c06c5
Dikshant <dikshantparasar@gmail.com>
2025-08-25 14:15:45 +0530

    Message Send Sucessfulyy Tested in Postman

--- Server/Routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const upload = require('../Models/multer');
-const { uploadImage } = require('../Controllers/uploadController');
-
-// POST /upload
-router.post('/upload', upload.single('image'), uploadImage);
-
-module.exports = router;
